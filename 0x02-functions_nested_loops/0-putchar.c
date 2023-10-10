f37df17d18ba3a3bb43897f82cb0cbf1d80b7491
Natadkab <nehemiahadane@gmail.com>
2023-10-10 15:27:32 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/*
+ * prints _putchar, followed by a new line.
+ *
+ * return 0 on sucess
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
